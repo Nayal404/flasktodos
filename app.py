e81681e3c69750333f0d7b924ec208cb5cd109fb
Nayal404 <imnayal404@gmail.com>
2022-07-20 10:16:15 +0545

    Added some Logics and some changes

--- app.py
+++ app.py
@@ -1,27 +1,24 @@
 from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///todo.db"
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todo.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
-
 class Todo(db.Model):
     sno = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(275), nullable=False)
-    password = db.Column(db.String(175), nullable=False)
-
+    title = db.Column(db.String(120), nullable=False)
+    desc = db.Column(db.String(500), nullable=False)
     def __repr__(self) -> str:
-        return f"{self.sno} - {self.email}"
-
+        return f'{self.sno}, {self.title}'
 @app.route('/', methods=['GET', 'POST'])
-def mainLogic():
-    if request.method== "POST":
-        userTitle= request.form['title']
-        userTodo = request.form['desc']
-        todo = Todo(email=userTitle, password=userTodo)
+def todoApp():
+    if request.method == 'POST':
+        todoTitle = request.form['title']
+        todoDesc = request.form['desc']
+        todo = Todo(title=todoTitle, desc=todoDesc)
         db.session.add(todo)
         db.session.commit()
     allTodo = Todo.query.all()
     return render_template('index.html', allTodo=allTodo)
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(debug=True)
--- static/js/script.js
+++ static/js/script.js
@@ -1,5 +1,14 @@
-console.log('JavaScript Working')
-let btn = document.querySelector('button');
-btn.onclick = document.addEventListener('click', ()=>{
-    document.getElementById('showTODOs').style.display = 'block';
-})
+// To verify wheather JS is executing or not?
+console.log('JavaScript Executed Successfully!!!');
+
+// If there is nothing in the table keep it be and try not to show the table data
+const todos = document.getElementById('todo');
+console.log(todos.row.length)
+function show(){
+    if (todos.row.length == 0){
+        todos.style.display='none'
+    }
+    else{
+        todos.style.display='block'
+    }
+}
